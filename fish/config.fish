04c181db085675b0f70c63c0c151cdefaf7296e1
pPrecel <filip.strozik@outlook.com>
2021-02-11 07:40:42 +0100

    Fix fish colors

--- fish/config.fish
+++ fish/config.fish
@@ -29,7 +29,7 @@ set -l dark_gray 747369
 set -l latte B3A06D
 
 # Syntax Highlighting Colors
-set -g fish_color_normal 'normal'
+set -g fish_color_normal normal
 set -g fish_color_command $green
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $gray
@@ -38,14 +38,14 @@ set -g fish_color_error $red
 set -g fish_color_param $lite_gray
 set -g fish_color_comment $yellow
 set -g fish_color_match $blue
-set -g fish_color_selection 'white --bold --background=brblack'
-set -g fish_color_search_match 'bryellow --background=brblack'
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match bryellow --background=brblack
 set -g fish_color_operator $blue
 set -g fish_color_escape $cyan
 set -g fish_color_autosuggestion $dark_gray
 
 # Completion Pager Colors
-set -g fish_pager_color_progress 'brwhite --background=cyan'
-set -g fish_pager_color_prefix 'white --bold --underline'
-set -g fish_pager_color_completion 'normal'
-set -g fish_pager_color_description "$latte yellow"
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_prefix white --bold --underline
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description $latte yellow
