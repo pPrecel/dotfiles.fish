92122e5c8d8368f2aeb3cafdc379b0111ed90ef5
pPrecel <filip.strozik@outlook.com>
2021-02-13 18:55:51 +0100

    Change cp to ln -f

--- scripts/common.fish
+++ scripts/common.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function info
-	echo [(set_color --bold blue) ' .. ' (set_color normal)] $argv
+	echo [(set_color --bold blue)' .. '(set_color normal)] $argv
 end
 
 function info_installation_complete
@@ -13,15 +13,15 @@ function info_installation_skipped
 end
 
 function ask
-	echo [(set_color --bold yellow) ' ?? ' (set_color normal)] $argv
+	echo [(set_color --bold yellow)' ?? '(set_color normal)] $argv
 end
 
 function success
-	echo [(set_color --bold green) ' OK ' (set_color normal)] $argv
+	echo [(set_color --bold green)' OK '(set_color normal)] $argv
 end
 
 function abort
-	echo [(set_color --bold red) ABRT (set_color normal)] $argv
+	echo [(set_color --bold red)ABRT(set_color normal)] $argv
 	exit 1
 end
 
@@ -32,8 +32,7 @@ function copyFishFolder
     cd $from
 
     for file in *.fish
-        cp $from/$file $to/$file
-        info "Copy complete to $to/$file"
+        copyWithBackup $from/$file $to/$file
     end
 end
 
@@ -43,10 +42,10 @@ function copyWithBackup
 
     # backup
     if test -f $to
-        cp $to $to.backup
-        info "Backup complete $to.backup"
+        cp $to $from.backup
+        info "Backup complete '$to.backup'"
     end
 
-    cp $from $to
-    info "Copy complete to $to"
+    ln -f $from $to
+    info "Copy complete to '$to'"
 end
