f4ebc3e01291cc075913202ae7b00c82914d7f9a
pPrecel <filip.strozik@outlook.com>
2021-02-14 14:47:20 +0100

    Add extensions for vim and tmux files

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,22 +2,34 @@
 
 function fish_prompt
     # $status gets nuked as soon as something else is run
-    set -l last_status $status
+    set -g last_status $status
 
-    if test -n "$SSH_TTY"
-        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
-    end
+    test -n "$SSH_TTY"
+        and __ssh_prompt
+        or __common_prompt
 
-    if test $last_status -eq 0
-        echo -n (set_color green)(prompt_pwd)' '
-    else
-        echo -n (set_color red)(prompt_pwd)' '
-    end
-
-    set_color -o
-    if test "$USER" = 'root'
-        echo -n (set_color red)'# '
-    end
-    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
     set_color normal
+end
+
+function __ssh_prompt
+    echo -n (set_color (__status_color))"$USER"(set_color white)'@'(set_color yellow)(pwd)' '
+end
+
+function __common_prompt
+    test "$USER" = 'root'
+        and echo -n (set_color (__status_color))'[ # ] '
+        or  echo -n (set_color (__status_color))(__status_symbol)' '
+    echo -n (set_color normal)(prompt_pwd)' '
+end
+
+function __status_color 
+    test $last_status -eq 0
+        and echo -n '78ab21'
+        or echo -n 'dc3936'
+end
+
+function __status_symbol
+    test $last_status -eq 0
+        and echo -n '[･‿･]'
+        or echo -n '[ಠ_ಠ]'
 end
