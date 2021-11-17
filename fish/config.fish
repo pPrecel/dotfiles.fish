995f753a614fc50a052594168844460377c07372
pPrecel <filip.strozik@outlook.com>
2021-11-17 14:07:59 +0100

    set nvim to be default editor

--- fish/config.fish
+++ fish/config.fish
@@ -4,14 +4,15 @@
 set fish_greeting
 
 set -gx GOPATH "$HOME/go"
-
 set -gx KCPCONFIG "$HOME/.kcp/config-dev.yaml"
+
+set -gx EDITOR=(which nvim)
 
 set -g fish_user_paths $fish_user_paths "$GOPATH/bin"
 set -g fish_user_paths $fish_user_paths "$HOME/google-cloud-sdk/bin"
 set -g fish_user_paths $fish_user_paths "$HOME/Library/Python/3.7/bin"
 set -g fish_user_paths $fish_user_paths "/opt/metasploit-framework/bin"
-set -g fish_user_paths $fish_user_paths "/usr/local/opt/tcl-tk/bin" 
+set -g fish_user_paths $fish_user_paths "/usr/local/opt/tcl-tk/bin"
 
 alias ls='lsd'
 
