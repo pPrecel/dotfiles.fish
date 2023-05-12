9d476ee4df9fd59d26504c85b72ae58701ef3373
pPrecel <filip.strozik@outlook.com>
2023-05-12 11:38:09 +0200

    upgrade vscode colors and fonts

--- vscode/init.fish
+++ vscode/init.fish
@@ -2,13 +2,13 @@
 
 source $argv[1]/scripts/common.fish
 
-if test -f /Library/Fonts/"DejaVu Sans Mono Nerd Font Complete.ttf"
-    info_installation_skipped "font 'DejaVu Sans Mono Nerd Font Complete.ttf'"
+if test -f /Library/Fonts/"DejaVuSansMNerdFont-Regular.ttf"
+    info_installation_skipped "DejaVuSansMNerdFont-Regular.ttf"
 else
-    curl -fLo /Library/Fonts/"DejaVu Sans Mono Nerd Font Complete.ttf" \
-        https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf \
+    curl -fLo /Library/Fonts/"DejaVuSansMNerdFont-Regular.ttf" \
+        https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/DejaVuSansMNerdFont-Regular.ttf \
         &>/dev/null
-    info_installation_complete "font 'DejaVu Sans Mono Nerd Font Complete.ttf'"
+    info_installation_complete "font 'DejaVuSansMNerdFont-Regular.ttf'"
 end
 
 lnWithBackup "$argv[1]/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
