50f3c6cf2bee6c0d22c345b4306daaff036bda62
pPrecel <filip.strozik@outlook.com>
2023-10-21 20:41:21 +0200

    abbr gr

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -11,6 +11,9 @@ abbr -a gcom 'git checkout master'
 abbr -a gp 'git push'
 abbr -a gfa 'git fetch --all'
 
+# improved review
+abbr -a -- gr 'git pull --no-commit --no-rebase'
+
 # diff-so-fancy
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 git config --global interactive.diffFilter "diff-so-fancy --patch"
